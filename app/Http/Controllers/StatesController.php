52d831a16b807dfca76289622c9dc2d9797a0638
Will <will@dejumbled.com>
2015-04-29 23:28:52 +0100

    Added 'states' end point

---
+++ app/Http/Controllers/StatesController.php
@@ -0,0 +1,59 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\State;
+use App\Http\Requests;
+use App\Http\Controllers\ApiController;
+use App\Transformers\StateTransformer;
+
+use Illuminate\Http\Request;
+
+class StatesController extends ApiController
+{
+    /**
+     * @var App\Transformers\StateTransformer
+     */
+    protected $stateTransformer;
+
+    /**
+     * Set the state transformer
+     *
+     * @param StateTransformer $stateTransformer
+     */
+    public function __construct(StateTransformer $stateTransformer)
+    {
+        $this->stateTransformer = $stateTransformer;
+    }
+
+    /**
+     * Output a list of states
+     *
+     * @param  Request $request
+     * @return mixed
+     */
+    public function index(Request $request)
+    {
+        return $this->respond([
+            'data' => $this->stateTransformer->transformCollection(State::get()->all())
+        ]);
+    }
+
+    /**
+     * Output details for a single state
+     *
+     * @param  integer $id
+     * @return mixed
+     */
+    public function show($id)
+    {
+        $state = State::find($id);
+
+        if (! $state) {
+            return $this->respondNotFound('State not found');
+        }
+
+        return $this->respond([
+            'data' => $this->stateTransformer->transform($state)
+        ]);
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,4 +9,5 @@ Route::group(['prefix' => 'api/v1'], function()
     Route::resource('competitions', 'CompetitionsController', ['only' => ['index', 'show']]);
     Route::resource('fixtures',     'FixturesController',     ['only' => ['index', 'show']]);
     Route::resource('results',      'ResultsController',      ['only' => ['index', 'show']]);
+    Route::resource('states',       'StatesController',       ['only' => ['index', 'show']]);
 });

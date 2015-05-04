99c45ce52c4d77ddc51878b37a8b9b053930cabf
Will <will@dejumbled.com>
2015-05-04 18:44:39 +0100

    Added Github fork ribbon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,9 +9,13 @@
         <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href="{{ asset('css/highlight/github.css') }}" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.1.1/gh-fork-ribbon.min.css" rel="stylesheet">
+        <!--[if lt IE 9]>
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.1.1/gh-fork-ribbon.ie.min.css" rel="stylesheet">
+        <![endif]-->
 
-        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
         <script src="{{ asset('js/highlight.pack.js') }}"></script>
         <script>hljs.initHighlightingOnLoad();</script>
@@ -20,6 +24,12 @@
     <body>
         <div class="container">
             @yield('content')
+        </div>
+
+        <div class="github-fork-ribbon-wrapper right">
+            <div class="github-fork-ribbon">
+                <a href="https://github.com/statsfc/statsfc-api">Fork me on GitHub</a>
+            </div>
         </div>
     </body>
 </html>

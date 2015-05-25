36ee9378293b81a17f1e1374268c88c9813a4df6
Will <will@dejumbled.com>
2015-05-25 01:47:36 +0100

    Return an error if the chosen competition is not in the subscription

--- app/Customer.php
+++ app/Customer.php
@@ -1,6 +1,7 @@
 <?php
 namespace App;
 
+use DB;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
@@ -20,5 +21,21 @@ class Customer extends Model
     public function rateLimiters()
     {
         return $this->hasMany('App\RateLimiter');
+    }
+
+    public function competitions($field = null)
+    {
+        $query = DB::table('competitions')
+            ->join('payment_competition', 'competitions.id', '=', 'payment_competition.competition_id')
+            ->join('payment', 'payment_competition.payment_id', '=', 'payment.id')
+            ->select('competitions.*')
+            ->where('payment.customer_id', $this->id)
+            ->where('competitions.online', true);
+
+        if ($field) {
+            return $query->lists('id');
+        }
+
+        return $query->get();
     }
 }
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,6 +2,7 @@
 namespace App\Http\Middleware;
 
 use App;
+use App\Competition;
 use App\Customer;
 use App\RateLimiter;
 use App\Http\Controllers\ApiController;
@@ -33,26 +34,71 @@ class Authenticate extends ApiController
 
         $customer = $customers->first();
 
-        if (App::environment() !== 'local') {
-            if ($customer->ip !== $request->ip()) {
-                return $this->respondUnauthorised('IP address does not match');
-            }
+        if (! $this->authenticateRequestIp($request, $customer)) {
+            return $this->respondUnauthorised('IP address does not match');
+        }
 
-            $rateLimiter = RateLimiter::firstOrCreate([
-                'customer_id' => $customer->id,
-                'date'        => Carbon::today()->toDateString()
-            ]);
+        if ($this->hasRequestedInvalidCompetition($request, $customer)) {
+            return $this->respondUnauthorised('The chosen competition is not in your API subscription');
+        }
 
-            if ($rateLimiter->calls >= RateLimiter::DAILY_LIMIT) {
-                return $this->respondTooManyRequests();
-            }
-
-            $rateLimiter->increment();
+        if ($this->hasExceededRateLimit($customer)) {
+            return $this->respondTooManyRequests();
         }
 
         // Put the customer into a session
         $request->session()->put('customer_id', $customer->id);
 
         return $next($request);
+    }
+
+    private function authenticateRequestIp($request, $customer)
+    {
+        if (App::environment() === 'local') {
+            return true;
+        }
+
+        return ($request->ip() === $customer->ip);
+    }
+
+    private function hasRequestedInvalidCompetition($request, $customer)
+    {
+        if ($request->has('competition')) {
+            $competitions = Competition::where('name', $request->input('competition'))->get();
+        } elseif ($request->has('competition_id')) {
+            $competitions = Competition::where('id', $request->input('competition_id'))->get();
+        } elseif ($request->has('competition_key')) {
+            $competitions = Competition::where('key', $request->input('competition_key'))->get();
+        } else {
+            return false;
+        }
+
+        if ($competitions->count() === 0) {
+            return false;
+        }
+
+        $competition = $competitions->first();
+
+        return (! in_array($competition->id, $customer->competitions('id')));
+    }
+
+    private function hasExceededRateLimit($customer)
+    {
+        if (App::environment() === 'local') {
+            return false;
+        }
+
+        $rateLimiter = RateLimiter::firstOrCreate([
+            'customer_id' => $customer->id,
+            'date'        => Carbon::today()->toDateString()
+        ]);
+
+        if ($rateLimiter->calls >= RateLimiter::DAILY_LIMIT) {
+            return true;
+        }
+
+        $rateLimiter->increment();
+
+        return false;
     }
 }

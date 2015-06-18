f116301bd6e64f3939816d2d8e0bbca58aec6d98
Will <will@dejumbled.com>
2015-06-18 23:12:21 +0100

    Added seasons JSON example

--- resources/views/examples/seasons.blade.php
+++ resources/views/examples/seasons.blade.php
@@ -4,3 +4,15 @@
     </span>
     <input type="text" class="form-control api-curl" aria-describedby="basic-addon1" value="curl -H &quot;X-StatsFC-Key: {api_key}&quot; https://dugout.statsfc.com/api/v1/seasons">
 </div>
+
+<pre><code class="json">{
+    "data": [
+        {
+            "id": 10,
+            "name": "2015\/2016",
+            "start": "2015-06-30",
+            "end": "2016-06-30"
+        },
+        …
+    ]
+}</code></pre>

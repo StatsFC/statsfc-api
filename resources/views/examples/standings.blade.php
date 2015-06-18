9541646e41c632f26c1ed94cd6054051b8ac0fc9
Will <will@dejumbled.com>
2015-06-18 23:46:34 +0100

    Added standings JSON example

--- resources/views/examples/standings.blade.php
+++ resources/views/examples/standings.blade.php
@@ -4,3 +4,38 @@
     </span>
     <input type="text" class="form-control api-curl" aria-describedby="basic-addon1" value="curl -H &quot;X-StatsFC-Key: {api_key}&quot; https://dugout.statsfc.com/api/v1/standings">
 </div>
+
+<pre><code class="json">{
+    "data": [
+        {
+            "competition": {
+                "id": 2,
+                "name": "Premier League",
+                "key": "EPL",
+                "region": "England"
+            },
+            "round": {
+                "id": 107,
+                "name": "Premier League",
+                "start": "2014-08-15",
+                "end": null
+            },
+            "team": {
+                "id": 7,
+                "name": "Chelsea",
+                "shortName": "Chelsea"
+            },
+            "position": 1,
+            "played": 38,
+            "wins": 26,
+            "draws": 9,
+            "losses": 3,
+            "for": 73,
+            "against": 32,
+            "difference": 41,
+            "points": 87,
+            "notes": null
+        },
+        …
+    ]
+}</code></pre>

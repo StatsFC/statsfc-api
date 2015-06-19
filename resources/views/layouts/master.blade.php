e586718a9a40a3e7c3b7b558171f48d4994846a5
Will <will@dejumbled.com>
2015-06-19 18:57:25 +0100

    Added a footer / removed GitHub fork link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,16 +9,7 @@
         <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href="{{ asset('css/highlight/github.css') }}" rel="stylesheet">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.1.1/gh-fork-ribbon.min.css" rel="stylesheet">
-        <!--[if lt IE 9]>
-            <link href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.1.1/gh-fork-ribbon.ie.min.css" rel="stylesheet">
-        <![endif]-->
-
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-
-        <script src="{{ asset('js/highlight.pack.js') }}"></script>
-        <script>hljs.initHighlightingOnLoad();</script>
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
     </head>
 
     <body>
@@ -26,10 +17,35 @@
             @yield('content')
         </div>
 
-        <div class="github-fork-ribbon-wrapper right">
-            <div class="github-fork-ribbon">
-                <a href="https://github.com/StatsFC/statsfc-api">Fork me on GitHub</a>
-            </div>
-        </div>
+        <footer class="text-center">
+            <ul class="list-inline">
+                <li>
+                    <a class="text-muted" href="https://twitter.com/StatsFC" target="_blank">
+                        <span class="sr-only">Twitter</span>
+                        <i class="fa fa-twitter fa-3x"></i>
+                    </a>
+                </li>
+                <li>
+                    <a class="text-muted" href="mailto:hello@statsfc.com">
+                        <span class="sr-only">Email</span>
+                        <i class="fa fa-envelope fa-3x"></i>
+                    </a>
+                </li>
+                <li>
+                    <a class="text-muted" href="https://github.com/StatsFC/statsfc-api" target="_blank">
+                        <span class="sr-only">GitHub</span>
+                        <i class="fa fa-github fa-3x"></i>
+                    </a>
+                </li>
+            </ul>
+
+            <p class="text-muted"><small>Copyright © {{ Carbon\Carbon::now()->year }} Stats FC</small></p>
+        </footer>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+        <script src="{{ asset('js/highlight.pack.js') }}"></script>
+        <script>hljs.initHighlightingOnLoad();</script>
     </body>
 </html>

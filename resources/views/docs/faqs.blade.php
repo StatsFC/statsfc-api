92d91cf19fd5949fb226eff46dbf318c0979e4a7
Will <will@dejumbled.com>
2015-06-21 19:36:36 +0100

    Make FAQs collapsable

--- resources/views/docs/faqs.blade.php
+++ resources/views/docs/faqs.blade.php
@@ -1,70 +1,80 @@
-<div class="panel panel-default">
-    <div class="panel-heading">Which competitions do you support?</div>
+<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+    <div class="panel panel-default">
+        <div class="panel-heading" role="tab" id="faq-heading-competitions">
+            <h4 class="panel-title">
+                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#faq-collapse-competitions" aria-expanded="true" aria-controls="faq-collapse-competitions">
+                    Which competitions do you support?
+                </a>
+            </h4>
+        </div>
 
-    <div class="panel-body">
-        <p>We support the following competitions, grouped by region:</p>
+        <div id="faq-collapse-competitions" class="panel-collapse collapse" role="tabpanel" aria-labeledby="faq-heading-competitions">
+            <div class="panel-body">
+                <p>We support the following competitions, grouped by region:</p>
 
-        <ul>
-            <li>
-                World
-                <ul>
-                    <li>FIFA Club World Cup</li>
+                <ul class="list-unstyled">
+                    <li>
+                        World
+                        <ul>
+                            <li>FIFA Club World Cup</li>
+                        </ul>
+                    </li>
+                    <li>
+                        Europe
+                        <ul>
+                            <li>UEFA Champions League</li>
+                            <li>UEFA Europa League</li>
+                            <li>UEFA Super Cup</li>
+                        </ul>
+                    </li>
+                    <li>
+                        England
+                        <ul>
+                            <li>Premier League</li>
+                            <li>Championship</li>
+                            <li>League One</li>
+                            <li>League Two</li>
+                            <li>FA Cup</li>
+                            <li>Football League Cup</li>
+                            <li>Football League Trophy</li>
+                            <li>FA Community Shield</li>
+                        </ul>
+                    </li>
+                    <li>
+                        Spain
+                        <ul>
+                            <li>Primera División <em>(La Liga)</em></li>
+                            <li>Copa del Rey</li>
+                            <li>Supercopa de España</li>
+                        </ul>
+                    </li>
+                    <li>
+                        Germany
+                        <ul>
+                            <li>Bundesliga</li>
+                            <li>DFB-Pokal</li>
+                            <li>DFL-Supercup</li>
+                        </ul>
+                    </li>
+                    <li>
+                        Italy
+                        <ul>
+                            <li>Serie A</li>
+                            <li>Coppa Italia</li>
+                            <li>Supercoppa Italiana</li>
+                        </ul>
+                    </li>
+                    <li>
+                        France
+                        <ul>
+                            <li>Ligue 1</li>
+                            <li>Coupe de France</li>
+                            <li>Coupe de la Ligue</li>
+                            <li>Trophée des Champions</li>
+                        </ul>
+                    </li>
                 </ul>
-            </li>
-            <li>
-                Europe
-                <ul>
-                    <li>UEFA Champions League</li>
-                    <li>UEFA Europa League</li>
-                    <li>UEFA Super Cup</li>
-                </ul>
-            </li>
-            <li>
-                England
-                <ul>
-                    <li>Premier League</li>
-                    <li>Championship</li>
-                    <li>League One</li>
-                    <li>League Two</li>
-                    <li>FA Cup</li>
-                    <li>Football League Cup</li>
-                    <li>Football League Trophy</li>
-                    <li>FA Community Shield</li>
-                </ul>
-            </li>
-            <li>
-                Spain
-                <ul>
-                    <li>Primera División <em>(La Liga)</em></li>
-                    <li>Copa del Rey</li>
-                    <li>Supercopa de España</li>
-                </ul>
-            </li>
-            <li>
-                Germany
-                <ul>
-                    <li>Bundesliga</li>
-                    <li>DFB-Pokal</li>
-                    <li>DFL-Supercup</li>
-                </ul>
-            </li>
-            <li>
-                Italy
-                <ul>
-                    <li>Serie A</li>
-                    <li>Coppa Italia</li>
-                    <li>Supercoppa Italiana</li>
-                </ul>
-            </li>
-            <li>
-                France
-                <ul>
-                    <li>Ligue 1</li>
-                    <li>Coupe de France</li>
-                    <li>Coupe de la Ligue</li>
-                    <li>Trophée des Champions</li>
-                </ul>
-            </li>
-        </ul>
+            </div>
+        </div>
     </div>
 </div>

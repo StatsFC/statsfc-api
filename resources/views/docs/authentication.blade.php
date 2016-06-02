1780ea0e1f725740a48d87e0fa7ed483e59acb56
Will <will@dejumbled.com>
2016-06-02 23:45:24 +0100

    Changed "StatsFC" to "Stats FC"

--- resources/views/docs/authentication.blade.php
+++ resources/views/docs/authentication.blade.php
@@ -1,4 +1,4 @@
-<p>You authenticate to the StatsFC API via API key.</p>
+<p>You authenticate to the Stats FC API via API key.</p>
 
 <p>Include the API key from your <a href="https://statsfc.com/account" target="_blank">account page</a> as a <code>X-StatsFC-Key</code> header. For example:</p>
 

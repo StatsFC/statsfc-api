e2bbf3e6ff0d71cb8b5658e93fde849554323b89
Will Woodward <will@willjw.co.uk>
2020-06-14 01:04:44 +0100

    Add v2 routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,16 +2,31 @@
 Route::group(['middleware' => 'maintenance'], function () {
     Route::get('/',    'HomeController@index');
     Route::get('docs', 'DocsController@index');
-    Route::get('test', 'V1\TestController@index');
 });
 
-Route::group(['prefix' => 'api/v1', 'middleware' => ['auth', 'db']], function () {
-    Route::resource('competitions', 'V1\CompetitionsController', ['only' => ['index']]);
-    Route::resource('fixtures',     'V1\FixturesController',     ['only' => ['index']]);
-    Route::resource('results',      'V1\ResultsController',      ['only' => ['index']]);
-    Route::resource('seasons',      'V1\SeasonsController',      ['only' => ['index']]);
-    Route::resource('squads',       'V1\SquadsController',       ['only' => ['index']]);
-    Route::resource('standings',    'V1\StandingsController',    ['only' => ['index']]);
-    Route::resource('states',       'V1\StatesController',       ['only' => ['index']]);
-    Route::resource('top-scorers',  'V1\TopScorersController',   ['only' => ['index']]);
-});
+switch (env('VERSION', '1')) {
+    case '1':
+        Route::group(['prefix' => 'api/v1', 'middleware' => ['auth', 'db']], function () {
+            Route::resource('competitions', 'V1\CompetitionsController', ['only' => ['index']]);
+            Route::resource('fixtures', 'V1\FixturesController', ['only' => ['index']]);
+            Route::resource('results', 'V1\ResultsController', ['only' => ['index']]);
+            Route::resource('seasons', 'V1\SeasonsController', ['only' => ['index']]);
+            Route::resource('squads', 'V1\SquadsController', ['only' => ['index']]);
+            Route::resource('standings', 'V1\StandingsController', ['only' => ['index']]);
+            Route::resource('states', 'V1\StatesController', ['only' => ['index']]);
+            Route::resource('top-scorers', 'V1\TopScorersController', ['only' => ['index']]);
+        });
+        break;
+
+    case '2':
+        Route::group(['prefix' => 'api/v2', 'middleware' => ['auth', 'db']], function () {
+            Route::resource('competitions', 'V1\CompetitionsController', ['only' => ['index']]);
+            Route::resource('fixtures', 'V1\FixturesController', ['only' => ['index']]);
+            Route::resource('results', 'V1\ResultsController', ['only' => ['index']]);
+            Route::resource('seasons', 'V1\SeasonsController', ['only' => ['index']]);
+            Route::resource('squads', 'V1\SquadsController', ['only' => ['index']]);
+            Route::resource('standings', 'V1\StandingsController', ['only' => ['index']]);
+            Route::resource('top-scorers', 'V1\TopScorersController', ['only' => ['index']]);
+        });
+        break;
+}

eb3604c1b3c88eea5380760e2361ef307cf34ed7
Will Woodward <will@willjw.co.uk>
2020-06-15 12:49:34 +0100

    v1 -> v2

--- resources/views/examples/standings.blade.php
+++ resources/views/examples/standings.blade.php
@@ -2,7 +2,7 @@
     <span class="input-group-addon" id="basic-addon1">
         <span class="glyphicon glyphicon-console" aria-hidden="true"></span>
     </span>
-    <input type="text" class="form-control api-curl" aria-describedby="basic-addon1" value="curl -H &quot;X-StatsFC-Key: {api_key}&quot; https://dugout.statsfc.com/api/v1/standings">
+    <input type="text" class="form-control api-curl" aria-describedby="basic-addon1" value="curl -H &quot;X-StatsFC-Key: {api_key}&quot; https://dugout.statsfc.com/api/v2/standings">
 </div>
 
 <pre><code class="json">{
